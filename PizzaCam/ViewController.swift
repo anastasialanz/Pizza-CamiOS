d442534314a9c62f32185d11ab785302578ef21f
Anastasia Lanz <anastasialanz@gmail.com>
2015-03-05 22:17:03 -0500

    Initial Commit

---
+++ PizzaCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaCam
+//
+//  Created by Anastasia Lanz on 3/5/15.
+//  Copyright (c) 2015 Wall to wall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
