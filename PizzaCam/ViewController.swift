b5f801acfa3ad8f824ddf54a98bb34e2b37943b6
Anastasia Lanz <anastasialanz@gmail.com>
2015-03-10 12:43:10 -0400

    Adds Photo Library access

--- PizzaCam/ViewController.swift
+++ PizzaCam/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    let picker = UIImagePickerController ()
+    
 
+    @IBAction func photoFromLibrary(sender: UIBarButtonItem) {
+        picker.allowsEditing = false //2
+        picker.sourceType = .PhotoLibrary //3
+        presentViewController(picker, animated: true, completion: nil) //4
+    }
+    
+    @IBAction func shootPhoto(sender: UIBarButtonItem) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        picker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: Delegates
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        var chosenImage = info[UIImagePickerControllerOriginalImage] as UIImage //2
+        myImageView.contentMode = .ScaleAspectFit //3
+        myImageView.image = chosenImage //4
+        dismissViewControllerAnimated(true, completion: nil) //5
+        
+        
+    }
+    //What to do if the image picker cancels.
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
+
 
